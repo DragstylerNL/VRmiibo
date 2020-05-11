1ee5d89d825a946ee238902b4a4c37b05f1ebeb3
blizzard190 <22256@ma-web.nl>
2020-05-11 13:52:32 +0200

    Made a sprite selector + a scene switch

---
+++ VRmiibo/Assets/Scripts/Login/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public InputField NameInputField;
+    public ImageViewer ImageViewer;
+
+    public void OnPlay(int scene)
+    {
+        if (NameInputField.text.Length >= 6)
+        {
+            //Put name to json thingy here
+            GoToScene(scene);
+        }
+    }
+    
+    public void GoToScene(int i)
+    {
+        SceneManager.LoadScene(i);
+    }
+}
---
+++ VRmiibo/Assets/Scripts/UI/ImageViewer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageViewer : MonoBehaviour
+{
+    public List<Sprite> Sprites = new List<Sprite>();
+
+    [SerializeField]private Image _image;
+    private int current = 0;
+
+    private void Start()
+    {
+        _image.sprite = Sprites[current];
+    }
+
+    public void OnNextImage()
+    {
+        current++;
+        if (current > Sprites.Count)
+            current = 0;
+        
+        _image.sprite = Sprites[current];
+    }
+
+    public void OnPreviousImage()
+    {
+        current--;
+        if (current < 0)
+            current = Sprites.Count;
+
+        _image.sprite = Sprites[current];
+    }
+}
