35f3b83c95dfe967dff364f1a621afba9350491b
blizzard190 <22256@ma-web.nl>
2020-05-20 11:15:16 +0200

    Added basic UI for Bumper Cars minigame

---
+++ VRmiibo/Assets/Scripts/UI/BumperCarsUI.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperCarsUI : MonoBehaviour
+{
+    [SerializeField]private GameObject _player;
+    private bool boosting = false;
+
+    public int cooldown;
+    
+    public void Left()
+    {
+        //Put reference to player right function
+    }
+
+    public void Right()
+    {
+        //Put reference to player left function
+    }
+
+    public void Boost()
+    {
+        if(!boosting)
+            StartCoroutine(Boosting());
+    }
+
+    private IEnumerator Boosting()
+    {
+        boosting = true;
+        //Put reference to player boost function
+        yield return new WaitForSeconds(cooldown);
+        boosting = false;
+    }
+}
