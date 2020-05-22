f43d1fe9f7e0d0defd03dfe6b496480745c2b81a
blizzard190 <22256@ma-web.nl>
2020-05-22 15:34:08 +0200

    Basic structure for minigame start

---
+++ VRmiibo/Assets/Scripts/MiniGames/StartGame/StartGame.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartGame : MonoBehaviour
+{
+    public int playerAmount;
+    
+    private NetworkClient _networkClient;//Network will need to send data when player enters the play zone to give the player a player number ID(Player 1, player 2, etc)
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            //other.getComponent<PlayerData>().GameID == playerAmount++;
+            //Call _networkclient player amount send
+        }
+    }
+}
