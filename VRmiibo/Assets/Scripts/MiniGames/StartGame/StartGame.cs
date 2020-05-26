70b2978dd652392601512fea84688c1d0920d159
blizzard190 <22256@ma-web.nl>
2020-05-26 11:20:08 +0200

    Finished Loading screen and began hub switch

--- VRmiibo/Assets/Scripts/MiniGames/StartGame/StartGame.cs
+++ VRmiibo/Assets/Scripts/MiniGames/StartGame/StartGame.cs
@@ -2,29 +2,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
-    public int playerAmount;
+    public int target;
     
     private NetworkClient _networkClient;//Network will need to send data when player enters the play zone to give the player a player number ID(Player 1, player 2, etc)
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     private void OnCollisionEnter(Collision other)
     {
         if (other.transform.CompareTag("Player"))
         {
-            //other.getComponent<PlayerData>().GameID == playerAmount++;
-            //Call _networkclient player amount send
+            /*Player enters ready zone, send state(ready/not ready) to server
+            Server updates the amount of players ready and the amount of players not ready and sends this back and shows it on the UI element
+            If enough players are ready start countdown for begining the game(Activate onGameStart)*/
         }
+    }
+
+    //Load load scene on button call
+    public void OnGameStart(int target)
+    {
+        SceneManager.LoadScene(2);
+        GameObject.FindWithTag("Loader").GetComponent<Loader>().LoadScene(target);
+        Destroy(this);
     }
 }
---
+++ VRmiibo/Assets/Scripts/MiniGames/StartGame/SwitchPlayArea.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPlayArea : MonoBehaviour
+{
+    public GameObject newPlayArea;
+
+    void Start()
+    {
+        GameObject oldArea = GameObject.FindWithTag("PlayArea");
+        Instantiate(newPlayArea, oldArea.transform);
+    }
+}
