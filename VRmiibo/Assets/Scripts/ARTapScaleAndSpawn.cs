29577147ee92b449538e1ff1527a8a4b5de47aa9
DragstylerNL <dragstylernl@gmail.com>
2020-05-27 14:05:42 +0200

    fixing

--- VRmiibo/Assets/Scripts/ARTapScaleAndSpawn.cs
+++ VRmiibo/Assets/Scripts/ARTapScaleAndSpawn.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Numerics;
-using System.Security.Policy;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.XR.ARFoundation;
