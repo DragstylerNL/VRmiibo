72ce53617875cd27868e3c2b5a27eed913666473
ColinvD <22950@ma-web.nl>
2020-05-28 17:00:13 +0200

    minor fixes

---
+++ VRmiibo/Assets/Scripts/UI/ButtonController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ButtonController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    private bool pointerDown;
+    public UnityEvent onPointerDown;
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        pointerDown = false;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        pointerDown = true;
+    }
+
+    private void Update()
+    {
+        if (pointerDown)
+        {
+            if (onPointerDown != null)
+            {
+                onPointerDown.Invoke();
+            }
+        }
+    }
+}
