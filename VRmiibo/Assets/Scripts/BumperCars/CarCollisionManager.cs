40ea7afb7edb3431398f8f55a0c3807acac12fd2
ColinvD <22950@ma-web.nl>
2020-05-28 11:31:27 +0200

    collision attempt

--- VRmiibo/Assets/Scripts/BumperCars/CarCollisionManager.cs
+++ VRmiibo/Assets/Scripts/BumperCars/CarCollisionManager.cs
@@ -7,9 +7,14 @@ public class CarCollisionManager : MonoBehaviour
     public void Bump(CarMovement car1, CarMovement car2)
     {
         Debug.Log("" + car1.name + " hit " + car2.name);
-        Vector3 TempDirCar1 = car1.Direction;
-        Vector3 TempDirCar2 = car2.Direction;
-        car1.Direction = TempDirCar2 + car1.transform.forward * -1 * bounceStrength;
-        car2.Direction = TempDirCar1 + car2.transform.forward * -1 * bounceStrength;
+        Vector3 TempDirCar1 = car1.CurrentDirection;
+        Vector3 TempDirCar2 = car2.CurrentDirection;
+        car1.CurrentDirection = TempDirCar2 + car1.transform.forward * -1 * bounceStrength;
+        car2.CurrentDirection = TempDirCar1 + car2.transform.forward * -1 * bounceStrength;
+    }
+
+    public void Bump(Vector3 direction, CarMovement car)
+    {
+        car.CurrentDirection = direction;
     }
 }
---
+++ VRmiibo/Assets/Scripts/BumperCars/CarTrigger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class CarTrigger : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name.Contains("Trigger") && name == "TriggerFront")
+        {
+            //FindObjectOfType<CarCollisionManager>().Bump(GetComponentInParent<CarMovement>().CurrentDirection, other.GetComponentInParent<CarMovement>());
+            //GetComponentInParent<CarMovement>().CurrentDirection = Vector3.back * 0.5f;
+            Debug.Log("" + name + " hit " + other.name);
+        }
+    }
+}
